5e61f2a0f2d9c48a101c8b85c390ea344b4bdb5e
Luca Ferrari <ferrari.luca@live.it>
2025-03-24 11:47:08 +0100

    remove folders

--- boundinspace/screen1/assets/arduino/WebSerialAPI_ui.js
+++
@@ -1,5 +0,0 @@
-const menu_button = document.getElementById("menu");
-
-menu_button.addEventListener('click', () => {
-    document.getElementById('serial_control_bar').classList.toggle('hidden');
-})
